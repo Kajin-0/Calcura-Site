9df4d75b7a8cc3f45c49b18721e5216bf3c13ca1
Kajin-0 <28634576+Kajin-0@users.noreply.github.com>
2026-09-17 09:53:02 -0400

    build: add classroom and contact page entries

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        classroom: resolve(__dirname, 'classroom/index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+      },
+    },
+  },
 });
