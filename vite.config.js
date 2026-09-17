a672b4677ff62873460eb688d36e24b954b411b4
Kajin-0 <28634576+Kajin-0@users.noreply.github.com>
2026-09-17 09:53:45 -0400

    fix: make multipage inputs ESM-safe

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,17 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const root = process.cwd();
+
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     rollupOptions: {
       input: {
-        home: resolve(__dirname, 'index.html'),
-        classroom: resolve(__dirname, 'classroom/index.html'),
-        contact: resolve(__dirname, 'contact/index.html'),
+        home: resolve(root, 'index.html'),
+        classroom: resolve(root, 'classroom/index.html'),
+        contact: resolve(root, 'contact/index.html'),
       },
     },
   },
